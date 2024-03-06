e504a721b0612b7c0db91b145a0c52e7f58ed98b
Anukanksha <anukanksha.priya2021@vitstudent.ac.in>
2024-03-06 16:07:20 +0530

    updated profile and logout page

--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -19,29 +19,43 @@ const Logout = () => {
         navigate(-1);
     };
 
+    const PageBackground = styled.div`
+    background-color: #85769f66;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
     return (
-        <LogoutContainer>
-            <h1>{currentUser.name}</h1>
-            <LogoutMessage>Are you sure you want to log out?</LogoutMessage>
-            <LogoutButtonLogout onClick={handleLogout}>Log Out</LogoutButtonLogout>
-            <LogoutButtonCancel onClick={handleCancel}>Cancel</LogoutButtonCancel>
-        </LogoutContainer>
+      <PageBackground>
+      <LogoutContainer>
+      <UserName>{currentUser.name}</UserName>
+          <LogoutMessage>Are you sure you want to log out?</LogoutMessage>
+          <ButtonContainer>
+              <LogoutButtonLogout onClick={handleLogout}>Log Out</LogoutButtonLogout>
+              <LogoutButtonCancel onClick={handleCancel}>Cancel</LogoutButtonCancel>
+          </ButtonContainer>
+      </LogoutContainer>
+  </PageBackground>
     );
 };
 
 export default Logout;
 
 const LogoutContainer = styled.div`
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
-  background-color: #85769f66;
-  color: black;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    padding: 40px;
+  
+    background-color: #fff; 
+    color: black;
+  
+    align-items: center;
+`;
+const UserName = styled.h1`
+   text-align: center;
+   font-size:2rem;
+   font-family: 'Times New Roman', Times, serif;
 `;
 
 const LogoutMessage = styled.p`
@@ -51,7 +65,7 @@ const LogoutMessage = styled.p`
 `;
 
 const LogoutButton = styled.button`
-  padding: 10px 20px;
+  padding: 6px 12px;
   margin-top: 10px;
   border-radius: 5px;
   font-size: 16px;
@@ -70,4 +84,10 @@ const LogoutButtonLogout = styled(LogoutButton)`
 
 const LogoutButtonCancel = styled(LogoutButton)`
   background-color: rgb(99, 60, 99);
+`;
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content:space-around;
+    width: 100%;
+    margin-top: 10px;
 `;
