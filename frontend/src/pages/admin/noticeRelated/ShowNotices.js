c3b33649c7f1e1f7e71dba1867e4aeec69013463
Saumyaa Prajapat <saumyaa.prajapat@gamil.com>
2024-04-19 13:34:17 +0530

    changes in add notice

--- frontend/src/pages/admin/noticeRelated/ShowNotices.js
+++ frontend/src/pages/admin/noticeRelated/ShowNotices.js
@@ -1,103 +1,139 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import {
-    Paper, Box, IconButton
-} from '@mui/material';
-import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import { Paper, Box, IconButton, Typography } from "@mui/material";
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { getAllNotices } from '../../../redux/noticeRelated/noticeHandle';
-import { deleteUser } from '../../../redux/userRelated/userHandle';
-import TableTemplate from '../../../components/TableTemplate';
-import { GreenButton,ButtonContainer } from '../../../components/buttonStyles';
-import SpeedDialTemplate from '../../../components/SpeedDialTemplate';
+import { getAllNotices } from "../../../redux/noticeRelated/noticeHandle";
+import { deleteUser } from "../../../redux/userRelated/userHandle";
+import TableTemplate from "../../../components/TableTemplate";
+import { GreenButton, ButtonContainer } from "../../../components/buttonStyles";
+import SpeedDialTemplate from "../../../components/SpeedDialTemplate";
+import nodata from "../../../assets/nodata.png";
 
 const ShowNotices = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { noticesList, loading, error, response } = useSelector(
+    (state) => state.notice
+  );
+  const { currentUser } = useSelector((state) => state.user);
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch();
-    const { noticesList, loading, error, response } = useSelector((state) => state.notice);
-    const { currentUser } = useSelector(state => state.user)
+  useEffect(() => {
+    dispatch(getAllNotices(currentUser._id, "Notice"));
+  }, [currentUser._id, dispatch]);
 
-    useEffect(() => {
-        dispatch(getAllNotices(currentUser._id, "Notice"));
-    }, [currentUser._id, dispatch]);
+  if (error) {
+    console.log(error);
+  }
 
-    if (error) {
-        console.log(error);
-    }
+  const deleteHandler = (deleteID, address) => {
+    dispatch(deleteUser(deleteID, address)).then(() => {
+      dispatch(getAllNotices(currentUser._id, "Notice"));
+    });
+  };
 
-    const deleteHandler = (deleteID, address) => {
-        dispatch(deleteUser(deleteID, address))
-            .then(() => {
-                dispatch(getAllNotices(currentUser._id, "Notice"));
-            })
-    }
+  const noticeColumns = [
+    { id: "title", label: "Title", minWidth: 170 },
+    { id: "details", label: "Details", minWidth: 100 },
+    { id: "date", label: "Date", minWidth: 170 },
+  ];
 
-    const noticeColumns = [
-        { id: 'title', label: 'Title', minWidth: 170 },
-        { id: 'details', label: 'Details', minWidth: 100 },
-        { id: 'date', label: 'Date', minWidth: 170 },
-    ];
-
-    const noticeRows = noticesList && noticesList.length > 0 && noticesList.map((notice) => {
-        const date = new Date(notice.date);
-        const dateString = date.toString() !== "Invalid Date" ? date.toISOString().substring(0, 10) : "Invalid Date";
-        return {
-            title: notice.title,
-            details: notice.details,
-            date: dateString,
-            id: notice._id,
-        };
+  const noticeRows =
+    noticesList &&
+    noticesList.length > 0 &&
+    noticesList.map((notice) => {
+      const date = new Date(notice.date);
+      const dateString =
+        date.toString() !== "Invalid Date"
+          ? date.toISOString().substring(0, 10)
+          : "Invalid Date";
+      return {
+        title: notice.title,
+        details: notice.details,
+        date: dateString,
+        id: notice._id,
+      };
     });
 
-    const NoticeButtonHaver = ({ row }) => {
-        return (
-            <>
-                <IconButton onClick={() => deleteHandler(row.id, "Notice")}>
-                    <DeleteIcon color="error" />
-                </IconButton>
-            </>
-        );
-    };
-
-    const actions = [
-        {
-            icon: <NoteAddIcon color="primary" />, name: 'Add New Notice',
-            action: () => navigate("/Admin/addnotice")
-        },
-        {
-            icon: <DeleteIcon color="error" />, name: 'Delete All Notices',
-            action: () => deleteHandler(currentUser._id, "Notices")
-        }
-    ];
-
+  const NoticeButtonHaver = ({ row }) => {
     return (
-        <>
-            {loading ?
-                <div>Loading...</div>
-                :
-                <>
-                    {response ?
-                        <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '16px' }}>
-                            <ButtonContainer>
-                            <GreenButton variant="contained"
-                                onClick={() => navigate("/Admin/addnotice")}>
-                                Add Notice
-                            </GreenButton></ButtonContainer>
-                        </Box>
-                        :
-                        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-                            {Array.isArray(noticesList) && noticesList.length > 0 &&
-                                <TableTemplate buttonHaver={NoticeButtonHaver} columns={noticeColumns} rows={noticeRows} />
-                            }
-                            <SpeedDialTemplate actions={actions} />
-                        </Paper>
-                    }
-                </>
-            }
-        </>
+      <>
+        <IconButton onClick={() => deleteHandler(row.id, "Notice")}>
+          <DeleteIcon color="error" />
+        </IconButton>
+      </>
     );
+  };
+
+  const actions = [
+    {
+      icon: <NoteAddIcon color="primary" />,
+      name: "Add New Notice",
+      action: () => navigate("/Admin/addnotice"),
+    },
+    {
+      icon: <DeleteIcon color="error" />,
+      name: "Delete All Notices",
+      action: () => deleteHandler(currentUser._id, "Notices"),
+    },
+  ];
+
+  return (
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {response ? (
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "16px",
+              }}
+            >
+              <ButtonContainer>
+                <GreenButton
+                  variant="contained"
+                  onClick={() => navigate("/Admin/addnotice")}
+                >
+                  Add Notice
+                </GreenButton>
+              </ButtonContainer>
+            </Box>
+          ) : (
+            <Paper sx={{ width: "100%", overflow: "hidden" }}>
+              {Array.isArray(noticesList) && noticesList.length > 0 ? (
+                <TableTemplate
+                  buttonHaver={NoticeButtonHaver}
+                  columns={noticeColumns}
+                  rows={noticeRows}
+                />
+              ) : (
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "300px",
+                  }}
+                >
+                  <img
+                    src={nodata}
+                    alt="No Data"
+                    style={{ maxWidth: "100%", maxHeight: "225px" }}
+                  />
+                  <Typography variant="h5">No notices found</Typography>
+                </Box>
+              )}
+              <SpeedDialTemplate actions={actions} />
+            </Paper>
+          )}
+        </>
+      )}
+    </>
+  );
 };
 
 export default ShowNotices;
