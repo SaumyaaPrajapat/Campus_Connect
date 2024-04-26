a85c0f94f24c873eec63591e8d33f569985da46f
SaumyaaPrajapat <saumyaa.prajapat@gmail.com>
2024-04-27 00:04:15 +0530

    Update ShowSubjects.js

--- frontend/src/pages/admin/subjectRelated/ShowSubjects.js
+++ frontend/src/pages/admin/subjectRelated/ShowSubjects.js
@@ -37,8 +37,6 @@ const ShowSubjects = () => {
   const deleteHandler = (deleteID, address) => {
     console.log(deleteID);
     console.log(address);
-    // setMessage("Sorry the delete function has been disabled for now.");
-    // setShowPopup(true);
 
     dispatch(deleteUser(deleteID, address))
         .then(() => {
