25c51850e920aaea237de5fa68f09492db65a8d4
SaumyaaPrajapat <saumyaa.prajapat@gmail.com>
2024-04-26 23:27:11 +0530

    Update ShowClasses.js

--- frontend/src/pages/admin/classRelated/ShowClasses.js
+++ frontend/src/pages/admin/classRelated/ShowClasses.js
@@ -54,10 +54,10 @@ const ShowClasses = () => {
     console.log(address);
     setMessage("Sorry the delete function has been disabled for now.");
     setShowPopup(true);
-    // dispatch(deleteUser(deleteID, address))
-    //   .then(() => {
-    //     dispatch(getAllSclasses(adminID, "Sclass"));
-    //   })
+    dispatch(deleteUser(deleteID, address))
+      .then(() => {
+        dispatch(getAllSclasses(adminID, "Sclass"));
+      })
   };
 
   const sclassColumns = [{ id: "name", label: "Class Name", minWidth: 170 }];
