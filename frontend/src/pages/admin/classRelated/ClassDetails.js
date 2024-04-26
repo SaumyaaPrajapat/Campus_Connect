b5161b401f2dcfa3e0b9ce2d35fee68d07f58fe9
SaumyaaPrajapat <saumyaa.prajapat@gmail.com>
2024-04-26 23:44:40 +0530

    Update ClassDetails.js

--- frontend/src/pages/admin/classRelated/ClassDetails.js
+++ frontend/src/pages/admin/classRelated/ClassDetails.js
@@ -79,8 +79,8 @@ const ClassDetails = () => {
     const deleteHandler = (deleteID, address) => {
         console.log(deleteID);
         console.log(address);
-        setMessage("Sorry the delete function has been disabled for now.")
-        setShowPopup(true)
+        // setMessage("Sorry the delete function has been disabled for now.")
+        // setShowPopup(true)
         dispatch(deleteUser(deleteID, address))
             .then(() => {
                 dispatch(getClassStudents(classID));
