8b198ac5bc7650861681052cad9c78e893eff4b3
SaumyaaPrajapat <saumyaa.prajapat@gmail.com>
2024-04-26 23:56:19 +0530

    Update ShowTeachers.js

--- frontend/src/pages/admin/teacherRelated/ShowTeachers.js
+++ frontend/src/pages/admin/teacherRelated/ShowTeachers.js
@@ -64,8 +64,8 @@ const ShowTeachers = () => {
   const deleteHandler = (deleteID, address) => {
     console.log(deleteID);
     console.log(address);
-    setMessage("Sorry the delete function has been disabled for now.");
-    setShowPopup(true);
+    // setMessage("Sorry the delete function has been disabled for now.");
+    // setShowPopup(true);
 
     dispatch(deleteUser(deleteID, address)).then(() => {
         dispatch(getAllTeachers(currentUser._id));
