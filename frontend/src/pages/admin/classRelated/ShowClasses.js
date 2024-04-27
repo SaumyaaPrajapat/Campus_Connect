9b535a749f5964252facbd9763f4ae721cb57283
Anukanksha Priya <102377700+cleveranu@users.noreply.github.com>
2024-04-27 15:55:55 +0530

    Update ShowClasses.js

--- frontend/src/pages/admin/classRelated/ShowClasses.js
+++ frontend/src/pages/admin/classRelated/ShowClasses.js
@@ -1,49 +1,36 @@
-import { useEffect, useState } from "react";
-import {
-  IconButton,
-  Box,
-  Menu,
-  MenuItem,
-  ListItemIcon,
-  Tooltip,
-} from "@mui/material";
+import { useEffect, useState } from 'react';
+import { IconButton, Box, Menu, MenuItem, ListItemIcon, Tooltip } from '@mui/material';
 import DeleteIcon from "@mui/icons-material/Delete";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { deleteUser } from "../../../redux/userRelated/userHandle";
-import { getAllSclasses } from "../../../redux/sclassRelated/sclassHandle";
-import {
-  BlueButton,
-  GreenButton,
-  ButtonContainer,
-} from "../../../components/buttonStyles";
-import TableTemplate from "../../../components/TableTemplate";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { deleteUser } from '../../../redux/userRelated/userHandle';
+import { getAllSclasses } from '../../../redux/sclassRelated/sclassHandle';
+import { BlueButton, GreenButton } from '../../../components/buttonStyles';
+import TableTemplate from '../../../components/TableTemplate';
 
-import SpeedDialIcon from "@mui/material/SpeedDialIcon";
-import PostAddIcon from "@mui/icons-material/PostAdd";
-import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
-import AddCardIcon from "@mui/icons-material/AddCard";
-import styled from "styled-components";
-import SpeedDialTemplate from "../../../components/SpeedDialTemplate";
-import Popup from "../../../components/Popup";
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import AddCardIcon from '@mui/icons-material/AddCard';
+import styled from 'styled-components';
+import SpeedDialTemplate from '../../../components/SpeedDialTemplate';
+import Popup from '../../../components/Popup';
 
 const ShowClasses = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   const dispatch = useDispatch();
 
-  const { sclassesList, loading, error, getresponse } = useSelector(
-    (state) => state.sclass
-  );
-  const { currentUser } = useSelector((state) => state.user);
+  const { sclassesList, loading, error, getresponse } = useSelector((state) => state.sclass);
+  const { currentUser } = useSelector(state => state.user)
 
-  const adminID = currentUser._id;
+  const adminID = currentUser._id
 
   useEffect(() => {
     dispatch(getAllSclasses(adminID, "Sclass"));
   }, [adminID, dispatch]);
 
   if (error) {
-    console.log(error);
+    console.log(error)
   }
 
   const [showPopup, setShowPopup] = useState(false);
@@ -52,49 +39,37 @@ const ShowClasses = () => {
   const deleteHandler = (deleteID, address) => {
     console.log(deleteID);
     console.log(address);
-    dispatch(deleteUser(deleteID, address))
-      .then(() => {
-        dispatch(getAllSclasses(adminID, "Sclass"));
-      });
-  };
+    setMessage("Sorry the delete function has been disabled for now.")
+    setShowPopup(true)
+    // dispatch(deleteUser(deleteID, address))
+    //   .then(() => {
+    //     dispatch(getAllSclasses(adminID, "Sclass"));
+    //   })
+  }
 
-  const sclassColumns = [{ id: "name", label: "Class Name", minWidth: 170 }];
+  const sclassColumns = [
+    { id: 'name', label: 'Class Name', minWidth: 170 },
+  ]
 
-  const sclassRows =
-    sclassesList &&
-    sclassesList.length > 0 &&
-    sclassesList.map((sclass) => {
-      return {
-        name: sclass.sclassName,
-        id: sclass._id,
-      };
-    });
+  const sclassRows = sclassesList && sclassesList.length > 0 && sclassesList.map((sclass) => {
+    return {
+      name: sclass.sclassName,
+      id: sclass._id,
+    };
+  })
 
   const SclassButtonHaver = ({ row }) => {
     const actions = [
-      {
-        icon: <PostAddIcon />,
-        name: "Add Subjects",
-        action: () => navigate("/Admin/addsubject/" + row.id),
-      },
-      {
-        icon: <PersonAddAlt1Icon />,
-        name: "Add Student",
-        action: () => navigate("/Admin/class/addstudents/" + row.id),
-      },
+      { icon: <PostAddIcon />, name: 'Add Subjects', action: () => navigate("/Admin/addsubject/" + row.id) },
+      { icon: <PersonAddAlt1Icon />, name: 'Add Student', action: () => navigate("/Admin/class/addstudents/" + row.id) },
     ];
     return (
       <ButtonContainer>
-        <IconButton
-          onClick={() => deleteHandler(row.id, "Sclass")}
-          color="secondary"
-        >
+        <IconButton onClick={() => deleteHandler(row.id, "Sclass")} color="secondary">
           <DeleteIcon color="error" />
         </IconButton>
-        <BlueButton
-          variant="contained"
-          onClick={() => navigate("/Admin/classes/class/" + row.id)}
-        >
+        <BlueButton variant="contained"
+          onClick={() => navigate("/Admin/classes/class/" + row.id)}>
           View
         </BlueButton>
         <ActionMenu actions={actions} />
@@ -115,17 +90,15 @@ const ShowClasses = () => {
     };
     return (
       <>
-        <Box
-          sx={{ display: "flex", alignItems: "center", textAlign: "center" }}
-        >
+        <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
           <Tooltip title="Add Students & Subjects">
             <IconButton
               onClick={handleClick}
               size="small"
               sx={{ ml: 2 }}
-              aria-controls={open ? "account-menu" : undefined}
+              aria-controls={open ? 'account-menu' : undefined}
               aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
+              aria-expanded={open ? 'true' : undefined}
             >
               <h5>Add</h5>
               <SpeedDialIcon />
@@ -142,75 +115,56 @@ const ShowClasses = () => {
             elevation: 0,
             sx: styles.styledPaper,
           }}
-          transformOrigin={{ horizontal: "right", vertical: "top" }}
-          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
         >
           {actions.map((action) => (
             <MenuItem onClick={action.action}>
-              <ListItemIcon fontSize="small">{action.icon}</ListItemIcon>
+              <ListItemIcon fontSize="small">
+                {action.icon}
+              </ListItemIcon>
               {action.name}
             </MenuItem>
           ))}
         </Menu>
       </>
     );
-  };
+  }
 
   const actions = [
     {
-      icon: <AddCardIcon color="primary" />,
-      name: "Add New Class",
-      action: () => navigate("/Admin/addclass"),
+      icon: <AddCardIcon color="primary" />, name: 'Add New Class',
+      action: () => navigate("/Admin/addclass")
     },
     {
-      icon: <DeleteIcon color="error" />,
-      name: "Delete All Classes",
-      action: () => deleteHandler(adminID, "Sclasses"),
+      icon: <DeleteIcon color="error" />, name: 'Delete All Classes',
+      action: () => deleteHandler(adminID, "Sclasses")
     },
   ];
 
   return (
     <>
-      {loading ? (
+      {loading ?
         <div>Loading...</div>
-      ) : (
+        :
         <>
-          {getresponse ? (
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                marginBottom: "20px", // Adjust as needed
-              }}
-            >
-              <ButtonContainer>
-                <GreenButton
-                  variant="contained"
-                  onClick={() => navigate("/Admin/addclass")}
-                >
-                  Add Class
-                </GreenButton>
-              </ButtonContainer>
+          {getresponse ?
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: '16px' }}>
+              <GreenButton variant="contained" onClick={() => navigate("/Admin/addclass")}>
+                Add Class
+              </GreenButton>
             </Box>
-          ) : (
+            :
             <>
-              {Array.isArray(sclassesList) && sclassesList.length > 0 && (
-                <TableTemplate
-                  buttonHaver={SclassButtonHaver}
-                  columns={sclassColumns}
-                  rows={sclassRows}
-                />
-              )}
+              {Array.isArray(sclassesList) && sclassesList.length > 0 &&
+                <TableTemplate buttonHaver={SclassButtonHaver} columns={sclassColumns} rows={sclassRows} />
+              }
               <SpeedDialTemplate actions={actions} />
-            </>
-          )}
+            </>}
         </>
-      )}
-      <Popup
-        message={message}
-        setShowPopup={setShowPopup}
-        showPopup={showPopup}
-      />
+      }
+      <Popup message={message} setShowPopup={setShowPopup} showPopup={showPopup} />
+
     </>
   );
 };
@@ -219,26 +173,33 @@ export default ShowClasses;
 
 const styles = {
   styledPaper: {
-    overflow: "visible",
-    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+    overflow: 'visible',
+    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
     mt: 1.5,
-    "& .MuiAvatar-root": {
+    '& .MuiAvatar-root': {
       width: 32,
       height: 32,
       ml: -0.5,
       mr: 1,
     },
-    "&:before": {
+    '&:before': {
       content: '""',
-      display: "block",
-      position: "absolute",
+      display: 'block',
+      position: 'absolute',
       top: 0,
       right: 14,
       width: 10,
       height: 10,
-      bgcolor: "background.paper",
-      transform: "translateY(-50%) rotate(45deg)",
+      bgcolor: 'background.paper',
+      transform: 'translateY(-50%) rotate(45deg)',
       zIndex: 0,
     },
-  },
-};
+  }
+}
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+`;
