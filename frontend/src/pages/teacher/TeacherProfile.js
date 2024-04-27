9015c9c64d397c7cb6231fcd96f00102462a66dd
Anukanksha Priya <102377700+cleveranu@users.noreply.github.com>
2024-04-27 15:33:05 +0530

    Update TeacherProfile.js

--- frontend/src/pages/teacher/TeacherProfile.js
+++ frontend/src/pages/teacher/TeacherProfile.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 import { Card, CardContent, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
@@ -6,34 +6,36 @@ import { useSelector } from 'react-redux';
 const TeacherProfile = () => {
   const { currentUser, response, error } = useSelector((state) => state.user);
 
-  if (response) { console.log(response) }
-  else if (error) { console.log(error) }
+  if (response) {
+    console.log(response);
+  } else if (error) {
+    console.log(error);
+  }
 
-  const teachSclass = currentUser.teachSclass
-  const teachSubject = currentUser.teachSubject
-  const teachSchool = currentUser.school
+  const teachSclass = currentUser.teachSclass;
+  const teachSubject = currentUser.teachSubject;
+  const teachSchool = currentUser.school;
 
   return (
-    <>
-      <ProfileCard>
-        <ProfileCardContent>
-          <ProfileText>Name: {currentUser.name}</ProfileText>
-          <ProfileText>Email: {currentUser.email}</ProfileText>
-          <ProfileText>Class: {teachSclass.sclassName}</ProfileText>
-          <ProfileText>Subject: {teachSubject.subName}</ProfileText>
-          <ProfileText>School: {teachSchool.schoolName}</ProfileText>
-        </ProfileCardContent>
-      </ProfileCard>
-    </>
-  )
-}
+    <ProfileCard elevation={3}>
+      <ProfileCardContent>
+        <ProfileText variant="h5">Name: {currentUser.name}</ProfileText>
+        <ProfileText variant="subtitle1">Email: {currentUser.email}</ProfileText>
+        <ProfileText variant="subtitle1">Class: {teachSclass.sclassName}</ProfileText>
+        <ProfileText variant="subtitle1">Subject: {teachSubject.subName}</ProfileText>
+        <ProfileText variant="subtitle1">School: {teachSchool.schoolName}</ProfileText>
+      </ProfileCardContent>
+    </ProfileCard>
+  );
+};
 
-export default TeacherProfile
+export default TeacherProfile;
 
 const ProfileCard = styled(Card)`
   margin: 20px;
   width: 400px;
   border-radius: 10px;
+  font-family: 'Roboto', sans-serif; /* Set desired font family */
 `;
 
 const ProfileCardContent = styled(CardContent)`
